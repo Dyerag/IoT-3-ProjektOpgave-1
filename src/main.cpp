59d71b05e4f5e2c99a20afcd4c403c2448873830
Tec <Tec@DESKTOP-2VA4957>
2025-11-19 09:17:11 +0100

    Breadboard layout tests are done. the button and lights work.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include <Arduino.h>
 
-int button = 2;
-int led = 25;
+int happyButton = 4;
+int happyLed = 26;
+int satisfiedButton = 33;
+int satisfiedLed = 32;
+int unsatisfiedButton = 34;
+int unsatisfiedLed = 14;
+int angryButton = 12;
+int angryLed = 25;
 
 // put function declarations here:
 
@@ -10,18 +16,51 @@ void setup()
   // put your setup code here, to run once:
   Serial.begin(115200);
   delay(1000);
-  pinMode(button, INPUT);
-  pinMode(led, OUTPUT);
+
+  pinMode(happyButton, INPUT);
+  pinMode(happyLed, OUTPUT);
+  pinMode(satisfiedButton, INPUT);
+  pinMode(satisfiedLed, OUTPUT);
+  pinMode(unsatisfiedButton, INPUT);
+  pinMode(unsatisfiedLed, OUTPUT);
+  pinMode(angryButton, INPUT);
+  pinMode(angryLed, OUTPUT);
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-  if (digitalRead(button) == HIGH)
-    // digitalWrite(led, HIGH);
-    Serial.println("Button pressed");
-  // else
-    // digitalWrite(led, LOW);
+  if (digitalRead(happyButton) == HIGH)
+  {
+    Serial.println("happyButton pressed");
+    digitalWrite(happyLed, HIGH);
+  }
+  else
+    digitalWrite(happyLed, LOW);
+
+  if (digitalRead(satisfiedButton) == HIGH)
+  {
+    Serial.println("satisfiedButton pressed");
+    digitalWrite(satisfiedLed, HIGH);
+  }
+  else
+    digitalWrite(satisfiedLed, LOW);
+
+  if (digitalRead(unsatisfiedButton) == HIGH)
+  {
+    Serial.println("unsatisfiedButton pressed");
+    digitalWrite(unsatisfiedLed, HIGH);
+  }
+  else
+    digitalWrite(unsatisfiedLed, LOW);
+
+  if (digitalRead(angryButton) == HIGH)
+  {
+    Serial.println("angryButton pressed");
+    digitalWrite(angryLed, HIGH);
+  }
+  else
+    digitalWrite(angryLed, LOW);
 }
 
 // put function definitions here:
